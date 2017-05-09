c81b7ef4e46d4c8c0203038295a5d92d44775e9c
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-05-08 18:38:36 -0700

    added navigation with event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  loadedFeature = 'recipe';
+  onSelected(feature) {
+    this.loadedFeature = feature;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() selected = new EventEmitter<string>();
+  onNavSelect(feature) {
+  	this.selected.emit(feature);
+  }
 
   constructor() { }
 
